3c5e25bde9f22126690d0c36e869d66bd4b9a9f7
JVO-CHILE <jvo.chile@gmail.com>
2016-11-04 00:09:29 -0300

    Implementación de programa que calcula la edad a partir de la fecha de nacimiento
    Pasos
    Verificar años bisiestos
    Verificar si año de nacimiento y año actual cuenta como bisiesto
    Calcula la cantidad de días vividos
    Restarle los bisiestos
    Dividirlo por 365
    Obtenemos la edad

--- CalculaEdad/Program.cs
+++ CalculaEdad/Program.cs
@@ -8,8 +8,48 @@ namespace CalculaEdad
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)        
         {
+            DateTime fechaNacimiento1 = new DateTime(1985, 11, 03);
+            DateTime fechaNacimiento2 = new DateTime(1985, 11, 04);
+            DateTime fechaNacimiento3 = new DateTime(1985, 11, 05);
+            
+            Console.WriteLine(calcularEdad(fechaNacimiento1));
+            Console.WriteLine(calcularEdad(fechaNacimiento2));
+            Console.WriteLine(calcularEdad(fechaNacimiento3));
+
+            Console.ReadKey();
+        }
+        
+        private static int cantidadBisiestos(DateTime fechaNacimiento)
+        {
+            int cantBisiestos = 0;
+            DateTime now = DateTime.Now;
+
+            for (int i = fechaNacimiento.Year; i <= DateTime.Now.Year; i++)
+            {
+                if (DateTime.IsLeapYear(i))
+                {
+                    cantBisiestos++;
+
+                    if (fechaNacimiento.Year.Equals(i))
+                        if (!(fechaNacimiento.Month <= 2))
+                            cantBisiestos--;          
+                }
+            }
+
+            return cantBisiestos;
+        }
+
+        private static int calcularEdad(DateTime fechaNacimiento)
+        {
+            DateTime aux = fechaNacimiento;
+            int cantDias=0, edad=0;
+
+            cantDias = (DateTime.Today - fechaNacimiento).Days;
+            edad = (cantDias - cantidadBisiestos(fechaNacimiento)) / 365;
+
+            return edad;
         }
     }
 }
